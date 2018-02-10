3aed6e2ca768cfcf995528e7d933cea3dea9da46
alon <skrops.a@gmail.com>
2018-02-10 21:06:07 +0200

    c

--- src/org/usfirst/frc/team3388/robot/TalonSpeed.java
+++ src/org/usfirst/frc/team3388/robot/TalonSpeed.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3388.robot;
+import com.ctre.CANTalon;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -29,5 +31,11 @@ public class TalonSpeed implements FlashSpeedController{
 	public boolean isInverted() {
 		return this.currInverted;
 	}
+	
+	public WPI_TalonSRX getSource()
+	{
+		return controller;
+	}
+	
 
 }
