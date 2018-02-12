50b067f4f65ddd10ea34ddb4c8df32aa03b9c7b3
Daniel Mikhailov <s16270@ganyavne.ort.org.il>
2018-02-12 14:44:37 +0200

    fixed rotatePIDAction

--- src/org/usfirst/frc/team3388/actions/RotatePIDAction.java
+++ src/org/usfirst/frc/team3388/actions/RotatePIDAction.java
@@ -2,12 +2,13 @@ package org.usfirst.frc.team3388.actions;
 
 import org.usfirst.frc.team3388.robot.Robot;
 
+import edu.flash3388.flashlib.math.Mathf;
 import edu.flash3388.flashlib.robot.Action;
 import edu.flash3388.flashlib.util.FlashUtil;
 
 public class RotatePIDAction extends Action {
 	public final static double MARGIN=2.5;
-	public final static double TIME_IN_THRESHOLD=3.0;
+	public final static double TIME_IN_THRESHOLD=2.0;
 	double thresholdStartTime
 	,startTime;
 	double setpoint;
@@ -19,30 +20,43 @@ public class RotatePIDAction extends Action {
 	@Override
 	protected void initialize() {
 		startTime=FlashUtil.secs();
+		Robot.drive.distancePID.reset();
+		Robot.drive.distancePID.setEnabled(true);
+		setpoint = Robot.drive.distanceSetPoint.get();
+		Robot.drive.resetGyro();
 	}
 	@Override
 	protected void end() {
-		Robot.drive.driveTrain.rotate(0.0);
+		Robot.drive.driveTrain.tankDrive(0.0,0.0);
+		Robot.drive.distancePID.setEnabled(false);
 	}
 
 	@Override
 	protected void execute() {
-		if(Robot.drive.rotationSource.pidGet()<=setpoint+MARGIN &&
-				Robot.drive.rotationSource.pidGet()>setpoint-MARGIN)
+		if(!Robot.drive.distancePID.isEnabled() || inDistanceThreshold())
 		{
-			if(thresholdStartTime!=0)
+			if(thresholdStartTime < 1)
 				thresholdStartTime=FlashUtil.secs();
-			Robot.drive.rotate(Robot.drive.rotatePID.calculate());
+			Robot.drive.drive(-Robot.drive.distancePID.calculate());	
+			
 		}
 		else
 		{
-			Robot.drive.rotate(Robot.drive.rotatePID.calculate());
-			thresholdStartTime=0;
+			if(thresholdStartTime >= 1)
+				thresholdStartTime = 0;
+			Robot.drive.drive(-Robot.drive.distancePID.calculate());		
 		}
+		
 	}
 	
 	@Override
 	protected boolean isFinished() {
-		return FlashUtil.secs()-thresholdStartTime>=TIME_IN_THRESHOLD && thresholdStartTime!=0		;
+		return inDistanceThreshold() && thresholdStartTime > 0 &&FlashUtil.secs() - thresholdStartTime >= TIME_IN_THRESHOLD;
+	}
+	
+	public boolean inDistanceThreshold() {
+		double current = Robot.drive.distancePID.getPIDSource().pidGet();
+		return Mathf.constrained(setpoint - current, -MARGIN, MARGIN);
+	
 	}
 }
